59918acd88be66df5afb99319b20d8606fd058ed
fjfox <fjfox@yandex.ru>
2020-07-03 16:03:41 +0300

    fix task in app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,18 @@
 const daysInWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'].reverse();
 const forDaysInWeek = document.querySelector('.schedule__week');
 const dayInWeek = document.querySelector('.schedule__tasks');
+let todayIs = new Date();
+
+const whatIsDay = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+
+
+
+    dayInWeek.dataset.day = whatIsDay[todayIs.getDay()];
+
+
+
+
+
 const schedule = {
     'пн': {
         '13:00' : 'Обед',
@@ -9,9 +21,27 @@ const schedule = {
     'вт': {
         '13:00' : 'Обед',
         '13:40' : 'Скайпкол'        
+    },
+    'пт': {
+        '13:00' : 'Обед',
+        '13:40' : 'Отдых'        
     }
 };
 
+
+
+let text = '';
+let toDayAdd = schedule[dayInWeek.dataset.day];
+for (let element in toDayAdd) {
+    text += `
+    <div class="day__task">
+    <div class = "day__time">${element}</div>
+    <div class = "day__reminder">${toDayAdd[element]}</div>
+    <div class ="task__delete">X</div>
+    </div>
+    `;
+}
+dayInWeek.innerHTML = text;
 
 
 for (let day in daysInWeek) {
@@ -23,27 +53,76 @@ for (let day in daysInWeek) {
     
     nowDay.addEventListener('click', (event) => {
         
-        let text = '';
+        dayInWeek.dataset.day = inday;
+        let toDayAdd = schedule[dayInWeek.dataset.day];
 
-        for (let element in schedule[inday]) {
+        let text = '';
+        for (let element in toDayAdd) {
             text += `
+            <div class="day__task">
             <div class = "day__time">${element}</div>
-            <div class = "day__reminder">${schedule[inday][element]}</div>
+            <div class = "day__reminder">${toDayAdd[element]}</div>
+            </div>
             `;
         }
         dayInWeek.innerHTML = text;
+
+
+
     });
 }
 
 
 
-
-
+const scheduleInputTime = document.querySelector('.schedule__input-time');
+const scheduleInputRemind = document.querySelector('.schedule__input-remind');
 const addTaskButton = document.querySelector('.schedule__button');
 
 addTaskButton.addEventListener('click', (event) => {
 
     event.preventDefault();
+    dayInWeek.insertAdjacentHTML('afterbegin', 
+    `       <div class="day__task">
+            <div class = "day__time">${scheduleInputTime.value}</div>
+            <div class = "day__reminder">${scheduleInputRemind.value}</div>
+            </div>
+            `
+    );
+
+    let toDayAdd = schedule[dayInWeek.dataset.day];
+
+    
+    toDayAdd[scheduleInputTime.value] = scheduleInputRemind.value;
+    
+    
+
+    let text = '';
+    for (let element in toDayAdd) {
+        text += `
+        <div class="day__task">
+        <div class = "day__time">${element}</div>
+        <div class = "day__reminder">${toDayAdd[element]}</div>
+        </div>
+        `;
+    }
+    dayInWeek.innerHTML = text;
+
+
+
+});
+
+
+
+
+dayInWeek.addEventListener('click', (event) => {
+    if (event.target.classList.contains('task__delete')) {
+        let timeToDelete = event.target.closest('.day__task').querySelector('.day__time').textContent;
+        timeToDelete
+
+        event.target.closest('.day__task').remove();
+        delete schedule[dayInWeek.dataset.day][timeToDelete];
+    } else if ()
+
 
 
 });
